6570bdf4e08e912933920c2377c00d4d7c04c248
Jarod Wakeman <jarodwakeman@gmail.com>
2023-03-16 13:48:47 -0400

    Movement Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,22 +9,38 @@ public class Player : MonoBehaviour
     float horizontalMove = 0f;
     public float runSpeed = 40f;
     bool jump = false;
-   
+    public Animator animator;
+
+
+    private void Awake()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
             
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
+
+        
     }
 }
